5d7bec7b0c927222877eceb2fa6ce1c49c10ecf7
vaishnavasokan <vaishnavasokan050>
2019-08-20 00:04:55 +0530

    4

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 app.set("views","./src/views");
 app.set("view engine","ejs");
 
-app.listen("8080",function()
+app.listen(process.env.PORT || "8080",function()
 {
     console.log("Server started.");
 })
